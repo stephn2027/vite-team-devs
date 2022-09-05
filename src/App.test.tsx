62ee6cad6aae5e358dcb293c216cfa2890a6c3c5
Takatoshi Wada <fueledbytaching@gmail.com>
2022-09-05 12:16:22 +0800

    initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,36 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { WrappedApp, App } from './App';
+
+describe('App', () => {
+  it('Renders hello world', () => {
+    // ARRANGE
+    render(<WrappedApp />);
+    // ACT
+    // EXPECT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello World');
+  });
+
+  it('Renders not found if invalid path', () => {
+    // ARRANGE
+    render(
+      <MemoryRouter initialEntries={['/this-route-is-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+    // ACT
+    // EXPECT
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
+  });
+});
