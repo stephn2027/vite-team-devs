e3aad0fe26bbf61dc47eb95d89b6b67d9c31fa87
Takatoshi Wada <fueledbytaching@gmail.com>
2022-09-05 15:35:12 +0800

    added: daisyUI, tailwindCSS config

---
+++ postcss.config.cjs
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
+module.exports = {
+  plugins: [require('tailwindcss'), require('autoprefixer')],
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button type="button" className="btn btn-primary">
+        DaisyUI Button
+      </button>
+    </>
+  );
 }
 
 export default Home;
