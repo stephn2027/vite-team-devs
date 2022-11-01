84976c1397fcf8295afe35000880ef364bd4c3f5
Takatoshi Wada <fueledbytaching@gmail.com>
2022-11-01 15:51:25 +0800

    feat: some fixes and update on other components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable react/require-default-props */
 import './style.scss';
 
 type ButtonProps = {
   text: string;
+  style?: React.CSSProperties | undefined;
 };
 
-export default function Button({ text }: ButtonProps) {
+export default function Button({ text, style }: ButtonProps) {
   return (
-    <div className="button-style">
+    <div className="button-style" style={style}>
       <span>{text}</span>
     </div>
   );
--- src/components/Slides/WhatWeDone/Project.tsx
+++ src/components/Slides/WhatWeDone/Project.tsx
@@ -5,7 +5,6 @@ interface ProjectProps {
   project: {
     name: string;
     solution: string;
-    country: string;
     tech: string;
     id: number;
   };
@@ -16,8 +15,13 @@ export default function Project({ project }: ProjectProps) {
     <div className="card w-96 shadow-xl project-style">
       <div className="card-body">
         <h2 className="card-title">{project.name}</h2>
-        <p>Solution: {project.solution}</p>
-        <div className="card-actions justify-start">
+        <p className="mt-2">
+          Solution: <span>{project.solution}</span>
+        </p>
+        <p className="mt-2">
+          Tech Stack: <span>{project.tech}</span>
+        </p>
+        <div className="card-actions justify-start mt-8">
           <Button text="Learn More" />
         </div>
       </div>
