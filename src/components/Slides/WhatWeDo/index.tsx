469c498d5695ef623f542625522f19390b570fdf
Takatoshi Wada <fueledbytaching@gmail.com>
2022-11-01 18:18:12 +0800

    fix: refactored code and design

--- src/components/Slides/WhatWeDo/index.tsx
+++ src/components/Slides/WhatWeDo/index.tsx
@@ -1,17 +1,15 @@
 import './style.scss';
 import SlideContainer from '../SlideContainer';
-import InfoContent from './InfoContent';
-import StepsContent from './StepsContent';
+import Info from './Info';
+import Steps from './Steps';
 
 export default function WhatWeDo() {
   return (
     <SlideContainer>
       <div className="howWeDoStyle w-screen h-screen">
-        <div className="wwd-style_content wwd-style_info">
-          <InfoContent />
-        </div>
-        <div className="wwd-style_content wwd-style_info">
-          <StepsContent />
+        <div className="howWeDoStyle_container">
+          <Info />
+          <Steps />
         </div>
       </div>
     </SlideContainer>
