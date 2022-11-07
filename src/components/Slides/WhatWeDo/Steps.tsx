60c1e456a83254a8664fc375e14c85f224993475
stephen <stephn2027@gmail.com>
2022-11-07 14:18:46 +0900

    fixed steps responsive feature. added carousel to steps

--- src/components/Slides/WhatWeDo/Steps.tsx
+++ src/components/Slides/WhatWeDo/Steps.tsx
@@ -1,3 +1,4 @@
+import IndividualStep from './IndividualStep';
 import STEPS_TEXT from '../../../assets/constants/steps';
 import './style.scss';
 
@@ -5,20 +6,8 @@ export default function Steps() {
   return (
     <div className="steps steps-style">
       <ul className="steps steps-vertical">
-        {STEPS_TEXT.map(({ id, title, description }) => (
-          <li
-            key={id}
-            className={`step steps-style-list ${
-              id === 1 ? 'step-secondary' : 'step-neutral'
-            }`}
-          >
-            <div>
-              <div className="steps-style_left">
-                <h1 className="steps-style_left-title">{title}</h1>
-                <p className="steps-style_left-explain">{description}</p>
-              </div>
-            </div>
-          </li>
+        {STEPS_TEXT.map((step) => (
+          <IndividualStep key={step.id} step={step} />
         ))}
       </ul>
     </div>
--- src/components/Slides/WhatWeDo/index.tsx
+++ src/components/Slides/WhatWeDo/index.tsx
@@ -1,15 +1,23 @@
-import './style.scss';
+import { useEffect, useState } from 'react';
 import SlideContainer from '../SlideContainer';
 import Info from './Info';
 import Steps from './Steps';
+import StepsCarousel from './StepsCarousel';
+import './style.scss';
 
 export default function WhatWeDo() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 620;
+  useEffect(() => {
+    window.addEventListener('resize', () => setWidth(window.innerWidth));
+  }, [width]);
+
   return (
     <SlideContainer>
-      <div className="howWeDoStyle w-screen h-screen">
+      <div className="howWeDoStyle w-screen">
         <div className="howWeDoStyle_container">
           <Info />
-          <Steps />
+          {width > breakpoint ? <Steps /> : <StepsCarousel />}
         </div>
       </div>
     </SlideContainer>
